89970fa6d5a659d504b07cc3db54099907c5bc61
okidwijaya <okidwijaya@gmail.com>
2021-06-10 20:04:52 +0700

    import json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,15 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import * as json from '../DATA.json';
 
-import ('../DATA.json').then(({ default: jsonData }) => {
-    let dataRestaurant = jsonData['restaurants']
-    let dataList = '';
-    dataRestaurant.forEach(function(data) {
-        dataList += `
+const data = json.default.restaurants;
+let dataList = '';
+data.forEach(function(data) {
+    dataList += `
       <div class="card">
         <article>
             <img src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
-            <h4 class="city_name">kota.${data['city']}</h4>
+            <h4 class="city_name">kota ${data['city']}</h4>
             <h5>Rating : ${data['rating']}</h5>
             <h3>${data['name']}</h3>
             <p>${data['description'].slice(0, 150)}...</p>
@@ -17,6 +17,5 @@ import ('../DATA.json').then(({ default: jsonData }) => {
         </article>
     </div>
     `;
-    });
-    document.querySelector('#data-card').innerHTML = dataList;
 });
+document.querySelector('#data-card').innerHTML = dataList;
